cad1fc12489a34156818c6ea8abbe716f675523b
조윤식 <cho.yunsig56@gmail.com>
2025-06-20 14:17:53 +0900

    AuthRepository 설정

--- core/lib/domain/auth/auth_repository.dart
+++ core/lib/domain/auth/auth_repository.dart
@@ -3,11 +3,18 @@ import 'package:core/domain/auth/model/login_request.dart';
 
 abstract interface class AuthRepository {
   /// 로그인 여부
-  Future<bool> get isLoggedIn;
+  Future<Result<bool>> get isLoggedIn;
+
+  /// 회원가입이 완료되지 않은 상태인지에 대한 여부
+  Future<Result<bool>> get isSignUpPending;
 
   /// 로그인
+  /// 
+  /// 성공 여부를 반환한다.
   Future<Result<bool>> login({required LoginRequest request});
 
   /// 로그아웃
+  /// 
+  /// 성공 여부를 반환한다.
   Future<Result<bool>> logout();
 }
--- core/lib/domain/auth/model/auth_info.dart
+++ core/lib/domain/auth/model/auth_info.dart
@@ -1,3 +1,16 @@
-sealed class AuthInfo {
-  // empty
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_info.freezed.dart';
+
+@freezed
+abstract class AuthInfo with _$AuthInfo {
+  const factory AuthInfo({
+    required AuthProvider authProvider,
+    required AuthTokenType authTokenType,
+    required String accessToken,
+    required String? idToken,
+  }) = _AuthInfo;
 }
+
+enum AuthTokenType { bearer }
+enum AuthProvider { google, apple, kakao, naver }

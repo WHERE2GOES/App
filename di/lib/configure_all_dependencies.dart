0adf5f7ccc77971b32f6f3bf61fc04d3eef0005f
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-09-13 22:53:04 +0900

    환경변수 주입 로직 변경

--- di/lib/configure_all_dependencies.dart
+++ di/lib/configure_all_dependencies.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: library_prefixes
 
+import 'package:di/configure_env.dart';
 import 'package:home/di/configure_dependencies.dart' as configureHome;
 import 'package:login/di/configure_dependencies.dart' as configureLogin;
 import 'package:data/di/configure_dependencies.dart' as configureData;
 
 Future<void> configureAllDependencies() async {
+  await configureEnv();
+
   await configureData.configureDependencies();
   await configureHome.configureDependencies();
   await configureLogin.configureDependencies();
